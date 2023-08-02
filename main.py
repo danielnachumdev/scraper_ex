512847845f3b588982b6784cd11b7e3f44ce34f9
danielnachumdev <danielnachumdev@gmail.com>
2023-08-02 19:41:20 +0300

    ignored warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # sneaky way to access "module as main"'s logic without reimplementing it again
 # as I envision this being used as a module that can also be called directly
-from scraper.__main__ import main
+from scraper.__main__ import main  # pylint: disable=import-error
 
 if __name__ == "__main__":
     main()
